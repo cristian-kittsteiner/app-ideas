451c7537e030edfdf8e7b13f0d921d7bd0074c20
cristian-kittsteiner <cdkittsteiner@yahoo.com>
2023-04-27 20:17:15 -0400

    Functionality complete. styling time

--- Projects/1-Beginner/bin2dec/src/App.jsx
+++ Projects/1-Beginner/bin2dec/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import binToDec from "./functions";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0);
+  const [output, setOutput] = useState(0);
+
+  function onClick() {
+    setOutput(binToDec(input));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label htmlFor="Input a binary number">Input a binary number</label>
+      <input
+        type="text"
+        className="input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className="button" onClick={onClick}>
+        Calculate!
+      </button>
+      <p>Your converted value is {output}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Projects/1-Beginner/bin2dec/src/functions.js
@@ -0,0 +1,33 @@
+function binToDec(stringNum) {
+  let decimal = 0;
+  let exponent = 0;
+  let negExp = -1;
+  const decPoint = stringNum.indexOf(".");
+  if (decPoint !== -1) {
+    for (let i = decPoint - 1; i >= 0; i--) {
+      const number = stringNum[i];
+      decimal += number * Math.pow(2, exponent);
+      exponent++;
+    }
+    for (let i = decPoint + 1; i <= stringNum.length - 1; i++) {
+      const number = stringNum[i];
+      decimal += number * Math.pow(2, negExp);
+      negExp--;
+    }
+  } else {
+    for (let i = stringNum.length - 1; i >= 0; i--) {
+      const number = stringNum[i];
+      if (number == 0 || number == 1) {
+        decimal += number * Math.pow(2, exponent);
+        exponent++;
+      } else {
+        console.log("please only enter 1 or 0");
+        return;
+      }
+    }
+  }
+
+  return decimal;
+}
+
+export default binToDec;
